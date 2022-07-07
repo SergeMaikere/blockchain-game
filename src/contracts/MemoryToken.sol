540c03a6bf77d254f4353fec435f945143864180
SergeMaikere <serge.maikere@gmail.com>
2022-07-07 09:04:24 +0200

    fixed package conficts + testing enabled

--- src/contracts/MemoryToken.sol
+++ src/contracts/MemoryToken.sol
@@ -1,10 +1,11 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract MemoryToken is ERC721 {
 
-	constructor () ERC721("Memory Token", "MEMORY") public {
+	constructor () ERC721("Memory Token", "MEMORY") {
 
 	}
 }
--- test/MemoryToken.test.js
+++ test/MemoryToken.test.js
@@ -6,6 +6,41 @@ require('chai')
 
 contract(
 	'Memory Token', accounts => {
-		
+
+		let token;	
+
+		before ( 
+			async () => {
+				token = await MemoryToken.deployed();
+			}
+		)
+
+		describe('deployment', 
+			async () => {
+				it ('deploys successfully', 
+					async () => {
+						const address = token.address;
+						assert.notEqual(address, 0x0);
+						assert.notEqual(address, null);
+						assert.notEqual(address, 'undefined');
+						assert.notEqual(address, '');
+					}
+				)
+
+				it ('has a name',
+					async () => {
+						const name = await token.name();
+						assert.equal(name, 'Memory Token');
+					}
+				)
+
+				it ('has a symbol',
+					async () => {
+						const symbol = await token.symbol();
+						assert.equal(symbol, 'MEMORY');
+					}
+				)
+			}
+		)
 	}
 );
