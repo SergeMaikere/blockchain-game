546b413c61a9afc8c04ef918f2e0cb0ea725064e
SergeMaikere <serge.maikere@gmail.com>
2022-07-13 16:40:59 +0200

    get all account tokenURIs + token images added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,10 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Web3 from 'web3';
 import MemoryToken from '../abis/MemoryToken.json';
 import Navbar from './Navbar';
 
 
 const App = () => {
-
-	const [ account, setAccount ] = useState('0x0');
-	const [ contract, setContract ] = useState();
 
 	useEffect(
 		() => {
@@ -19,6 +15,12 @@ const App = () => {
 			init()
 		}, []
 	)
+
+	const [ account, setAccount ] = useState( '0x0' );
+	const [ token, setToken ] = useState( {} );
+	const [ totalSupply, setTotalSupply ] = useState( 0 );
+	const [ tokenURIS, setTokenURIS ] = useState( [] );
+
 
 	/**
 	 * Loads web3.
@@ -41,16 +43,26 @@ const App = () => {
 	const getBlockchainDatas = async web3 => {
 
 		//Get current account
-		const accounts = await web3.eth.getAccounts();
-		setAccount(accounts[0]);
+		const [ accounts, error ] = await web3.eth.getAccounts();
+		if (error) alert('no accounts detected');
+		setAccount(accounts);
 
 		//Get the contract
-		const myContract = await new web3.eth.Contract(MemoryToken.abi);
-		setContract(myContract);
+		const currentAddress = await getCurrentAddress(web3);
+		const myContract = await new web3.eth.Contract(MemoryToken.abi, currentAddress);
+		setToken(myContract);
+
+		//Get totalSupply
+		const supply = await myContract.methods.totalSupply().call();
+		setTotalSupply(supply);
+
+		//Get all tokens
+		const tokens = await getAllTokens(myContract, accounts);
+		setTokenURIS(tokens);
 	}
 
 	/**
-	 * Enables metamask.
+	 * Enables Metamask.
 	 *
 	 * @return     object   
 	 * Web3js is a collection of libraries 
@@ -62,10 +74,39 @@ const App = () => {
 		return new Web3(window.ethereum);
 	}
 
+	/**
+	 * Gets the current address.
+	 *
+	 * @param      object  web3    The web 3
+	 * @return     address  The current address.
+	 */
+	const getCurrentAddress = async web3 => {
+		const networkId = await web3.eth.net.getId();
+		return networkId ? MemoryToken.networks[networkId].address : alert('MemoryToken contract is not deployed.')
+	}
+
+	/**
+	 * Gets all tokens.
+	 *
+	 * @return     {Array}  All tokenURIs.
+	 */
+	const getAllTokens = async(contract, owner) => {
+		const balance = await contract.methods.balanceOf(owner).call();
+		let tokens = [];
+		for (let i = 0; i < balance; i++) {
+			const id = await contract.methods.tokebOfOwnerByIndex(account, i).call();
+			const tokenURI = await contract.methods.tokenURI(id).call();
+			tokens.push(tokenURI);
+		}
+		return tokens;
+	}
+
   	return (
     	<div className="App">
     		<Navbar account={account}/>
-    		<h1>Hello Worlds</h1>
+    		<div className="container-fluid">
+    			<p>{tokenURIS}</p>
+    		</div>
     	</div>
   	);
 }
